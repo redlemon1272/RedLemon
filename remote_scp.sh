d70d75eacc0da06e0b69930cf73c86b21d0c8f09
orangeapple1272 <orangeapple1272@proton.me>
2026-01-08 17:34:03 -0500

    chore: add remote_scp helper script

--- remote_scp.sh
+++ remote_scp.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/expect -f
 
-# usage: ./remote_scp.sh local_file remote_path
-
 set timeout 300
 set password "123Scarface123!"
-set local_file [lindex $argv 0]
-set remote_path [lindex $argv 1]
+set src [lindex $argv 0]
+set dest [lindex $argv 1]
 set host "151.243.109.243"
 
-spawn scp -o StrictHostKeyChecking=no $local_file root@$host:$remote_path
+spawn scp -o StrictHostKeyChecking=no $src root@$host:$dest
 
 expect {
     "password:" {

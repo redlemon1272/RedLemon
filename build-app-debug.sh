52ff466b65780be2b2ef5c9075b55f410e79841b
orangeapple1272 <orangeapple1272@proton.me>
2026-01-11 14:09:53 -0500

    Bump version to 1.0.58

--- build-app-debug.sh
+++ build-app-debug.sh
@@ -14,8 +14,8 @@ MACOS_VERSION=$(sw_vers -productVersion)
 XCODE_VERSION=$(xcodebuild -version | head -1 | awk '{print $2}')
 
 # User-configurable versioning
-APP_VERSION="1.0.57"
-APP_BUILD="57"
+APP_VERSION="1.0.58"
+APP_BUILD="58"
 
 echo "🍋 Building RedLemon.app (DEBUG mode - faster)..."
 echo "🔧 System: $ARCH_NAME"

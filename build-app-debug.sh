41b7a1dcd50e8d5100be384076d9a17be0cf4012
orangeapple1272 <orangeapple1272@proton.me>
2026-01-14 16:55:02 -0500

    chore: release v1.0.87

--- build-app-debug.sh
+++ build-app-debug.sh
@@ -14,8 +14,8 @@ MACOS_VERSION=$(sw_vers -productVersion)
 XCODE_VERSION=$(xcodebuild -version | head -1 | awk '{print $2}')
 
 # User-configurable versioning
-APP_VERSION="1.0.86"
-APP_BUILD="86"
+APP_VERSION="1.0.87"
+APP_BUILD="87"
 
 echo "🍋 Building RedLemon.app (DEBUG mode - faster)..."
 echo "🔧 System: $ARCH_NAME"

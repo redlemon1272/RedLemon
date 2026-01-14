a4e33110471707a0e07e986c262d874546f817ee
orangeapple1272 <orangeapple1272@proton.me>
2026-01-14 03:59:04 -0500

    chore: release v1.0.83 (build 83) - Fix Season Pack Unlocking

--- build-app-debug.sh
+++ build-app-debug.sh
@@ -14,8 +14,8 @@ MACOS_VERSION=$(sw_vers -productVersion)
 XCODE_VERSION=$(xcodebuild -version | head -1 | awk '{print $2}')
 
 # User-configurable versioning
-APP_VERSION="1.0.82"
-APP_BUILD="82"
+APP_VERSION="1.0.83"
+APP_BUILD="83"
 
 echo "🍋 Building RedLemon.app (DEBUG mode - faster)..."
 echo "🔧 System: $ARCH_NAME"

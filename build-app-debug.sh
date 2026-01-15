eef39b8e18f80c186025263ce7fa18a8ac67d190
orangeapple1272@proton.me <orangeapple1272>
2026-01-15 07:18:24 -0500

    Release: Prep v1.0.102 (Docs & Build Script)

--- build-app-debug.sh
+++ build-app-debug.sh
@@ -14,8 +14,8 @@ MACOS_VERSION=$(sw_vers -productVersion)
 XCODE_VERSION=$(xcodebuild -version | head -1 | awk '{print $2}')
 
 # User-configurable versioning
-APP_VERSION="v1.0.101"
-APP_BUILD="101"
+APP_VERSION="v1.0.102"
+APP_BUILD="102"
 
 echo "🍋 Building RedLemon.app (DEBUG mode - faster)..."
 echo "🔧 System: $ARCH_NAME"

4a1b90a61a3228aef7db9ce82fb31901acf9cbd6
orangeapple1272 <orangeapple1272@proton.me>
2026-01-14 21:45:16 -0500

    chore: bump version to 1.0.96

--- build-app-debug.sh
+++ build-app-debug.sh
@@ -14,8 +14,8 @@ MACOS_VERSION=$(sw_vers -productVersion)
 XCODE_VERSION=$(xcodebuild -version | head -1 | awk '{print $2}')
 
 # User-configurable versioning
-APP_VERSION="1.0.95"
-APP_BUILD="95"
+APP_VERSION="1.0.96"
+APP_BUILD="96"
 
 echo "🍋 Building RedLemon.app (DEBUG mode - faster)..."
 echo "🔧 System: $ARCH_NAME"

645ccf8cf9b7f8bf7fbd70723250da9b5352c045
orangeapple1272@proton.me <orangeapple1272>
2026-01-16 02:49:46 -0500

    chore: release v1.0.110 (prevent display sleep)

--- build-app-debug.sh
+++ build-app-debug.sh
@@ -14,8 +14,8 @@ MACOS_VERSION=$(sw_vers -productVersion)
 XCODE_VERSION=$(xcodebuild -version | head -1 | awk '{print $2}')
 
 # User-configurable versioning
-APP_VERSION="1.0.109"
-APP_BUILD="109"
+APP_VERSION="1.0.110"
+APP_BUILD="110"
 
 echo "🍋 Building RedLemon.app (DEBUG mode - faster)..."
 echo "🔧 System: $ARCH_NAME"

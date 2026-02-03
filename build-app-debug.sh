41eb21d55b38d0588d6233c36c8fe6abc9d4f877
orangeapple1272 <orangeapple1272@proton.me>
2026-02-02 19:06:20 -0500

    chore: release artifacts v1.0.187 (build 187)

--- build-app-debug.sh
+++ build-app-debug.sh
@@ -14,8 +14,8 @@ MACOS_VERSION=$(sw_vers -productVersion)
 XCODE_VERSION=$(xcodebuild -version | head -1 | awk '{print $2}')
 
 # User-configurable versioning
-APP_VERSION="v1.0.186"
-APP_BUILD="186"
+APP_VERSION="1.0.187"
+APP_BUILD="187"
 
 echo "🍋 Building RedLemon.app (DEBUG mode - faster)..."
 echo "🔧 System: $ARCH_NAME"
